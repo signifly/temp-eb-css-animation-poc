5b1fedabd88643ec884d1a5a74ecf4b334d64662
Frederik Fink Højriis <58032545+frederikfink@users.noreply.github.com>
2025-08-11 16:20:42 +0200

    add play component

--- src/lib/components/Play.svelte
+++ src/lib/components/Play.svelte
@@ -5,20 +5,35 @@
 </script>
 
 <div
-	class={`w-[30px] h-[30px] grid place-content-center bg-red-500 ${rounded && 'rounded-md'}`}
-	style={`background-color:${bgColor}; color:${color};`}
+	class={`w-[30px] h-[30px] grid place-content-center overflow-hidden relative ${rounded && 'rounded-md'}`}
+	style={`background-color:${bgColor}; color:${color}; --play-duration:2.8s;`}
 >
-	<svg
-		width="12"
-		height="13"
-		viewBox="0 0 12 13"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-		class="[animation:lens-flip_2.8s_linear_infinite]"
-	>
-		<path
-			d="M10.7068 5.87745C11.1347 6.12446 11.1347 6.74196 10.7068 6.98897L1.08085 12.5465C0.65303 12.7935 0.118253 12.4848 0.118253 11.9908L0.118254 0.875644C0.118254 0.381638 0.65303 0.0728853 1.08085 0.319888L10.7068 5.87745Z"
-			fill="#FF0000"
-		/>
-	</svg>
+	<div class="overflow-hidden w-3.5 h-3.5 relative">
+		<svg
+			width="14"
+			height="14"
+			viewBox="0 0 12 13"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			class="absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 will-change-transform [animation:play-button-fill_var(--play-duration)_linear_infinite]"
+		>
+			<path
+				d="M10.7068 5.87745C11.1347 6.12446 11.1347 6.74196 10.7068 6.98897L1.08085 12.5465C0.65303 12.7935 0.118253 12.4848 0.118253 11.9908L0.118254 0.875644C0.118254 0.381638 0.65303 0.0728853 1.08085 0.319888L10.7068 5.87745Z"
+				fill="currentColor"
+			/>
+		</svg>
+		<svg
+			width="14"
+			height="14"
+			viewBox="0 0 12 13"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			class="absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2 will-change-transform [animation:play-button-fill-reverse_var(--play-duration)_linear_infinite]"
+		>
+			<path
+				d="M10.7068 5.87745C11.1347 6.12446 11.1347 6.74196 10.7068 6.98897L1.08085 12.5465C0.65303 12.7935 0.118253 12.4848 0.118253 11.9908L0.118254 0.875644C0.118254 0.381638 0.65303 0.0728853 1.08085 0.319888L10.7068 5.87745Z"
+				fill="currentColor"
+			/>
+		</svg>
+	</div>
 </div>
